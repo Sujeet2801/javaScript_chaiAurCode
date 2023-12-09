4393ec3e825f40430665c46702d9132cdba1844d
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2023-12-09 14:32:13 +0000

    Updated object

--- 2_basics/3_objects.js
+++ 2_basics/3_objects.js
@@ -1,13 +1,13 @@
 // singleton
 // Object.create
 
-// object literals
+//****************** object literals *****************//
 
 const mySym = Symbol("key1")
 
 const JsUser = {
     name: "Sujeet",
-    "full name": "Sujeet Kumar Gupta",
+    "Full name": "Sujeet Kumar Gupta",
     [mySym]: "mykey1",
     age: 22,
     location: "Ghaziabad",
@@ -16,22 +16,24 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+console.log(JsUser.email)
+console.log(JsUser["email"])
+console.log(JsUser["full name"])
+console.log(JsUser[mySym])
+console.log(typeof mySym);
 
 JsUser.email = "sujeet@chatgpt.com"
 // Object.freeze(JsUser)
 JsUser.email = "sujeet@microsoft.com"
-// console.log(JsUser);
+console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
-JsUser.greetingTwo = function(){
+
+JsUser.greetingName = function(){
     console.log(`Hello JS user,${this.name}`);
 }
 
 console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+console.log(JsUser.greetingName());
