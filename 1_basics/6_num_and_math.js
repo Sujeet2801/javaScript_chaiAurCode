9ee9827b6d6507317c103e99fcfbebc81b5b7363
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2023-12-09 08:28:53 +0000

    Updated num and math

--- 1_basics/6_num_and_math.js
+++ 1_basics/6_num_and_math.js
@@ -1,15 +1,16 @@
+
 const score = 400
-// console.log(score);
+// console.log("Score: ",score);
 
-const balance = new Number(100)
-// console.log(balance);
+// const balance = new Number(100)
+// console.log("Balance: ",balance);
 
-// console.log(balance.toString().length);
+// console.log("Lenght: ",balance.toString().length);
 // console.log(balance.toFixed(1));
-// 
+
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+// // console.log(otherNumber.toPrecision(2));
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));
@@ -17,18 +18,20 @@ const hundreds = 1000000
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4, 3, 6, 8));
+// console.log("Absolute: ",Math.abs(-4));
+// console.log("Round: ",Math.round(4.7));
+// console.log("Ceil of 4.2: ",Math.ceil(4.2));
+// console.log("Floor of 4.9: ",Math.floor(4.9));
+// console.log("Min of 4,3,6,8: ",Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
-// console.log(Math.random());
-// console.log((Math.random()*10) + 1);
-// console.log(Math.floor(Math.random()*10) + 1);
+console.log("Random: ",Math.random()); //(lie b/w 0 & 1)
+// console.log("Random: ",(Math.random()*10) + 1);
+// console.log("",Math.floor(Math.random()*10) + 1);
+// console.log("Random: ",(Math.random()*10) + 1);
+// console.log("",Math.floor(Math.random()*10) + 1);
 
-// const min = 10
+const min = 10
 const max = 20
 
-// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
