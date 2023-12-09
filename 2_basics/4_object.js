e3be78d47f53cb92806c16fd3b608fda21c68d05
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2023-12-09 17:51:37 +0000

    objects

--- 2_basics/4_object.js
+++ 2_basics/4_object.js
@@ -1,34 +1,35 @@
+
 // const tinderUser = new Object()
 const tinderUser = {}
+console.log(typeof tinderUser);
 
-tinderUser.id = "123abc"
-tinderUser.name = "Sammy"
+tinderUser.id = "280102"
+tinderUser.name = "Sujeet"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+console.log(tinderUser);
 
 const regularUser = {
     email: "some@gmail.com",
     fullname: {
         userfullname: {
-            firstname: "hitesh",
-            lastname: "choudhary"
+            firstname: "sujeet",
+            lastname: "gupta"
         }
     }
 }
-
+// console.log(regularUser.email);
 // console.log(regularUser.fullname.userfullname.firstname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
+// const obj5 = { obj1, obj2 }
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
-
+const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3);
 
 const users = [
     {
@@ -54,6 +55,7 @@ users[1].email
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+/*************** Object Destructing *****************/
 
 const course = {
     coursename: "js in hindi",
@@ -63,10 +65,10 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+// const {courseInstructor: instructor} = course
 
 // console.log(courseInstructor);
-console.log(instructor);
+// console.log(instructor);
 
 // {
 //     "name": "hitesh",
