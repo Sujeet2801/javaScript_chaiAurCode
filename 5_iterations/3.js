303aadb49d10895ef664171c067b4e1264a72e9e
Sujeet Kumar Gupta <118064339+Sujeet2801@users.noreply.github.com>
2023-12-28 07:12:14 +0000

    3.js

--- 5_iterations/3.js
+++ 5_iterations/3.js
@@ -6,24 +6,26 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    //console.log(num);
+    // console.log(num);
 }
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    // if(greet === " "){
+    //     continue;
+    // }
+    // console.log(`Each char is ${greet}`)
 }
 
 // Maps
 
-const map = new Map()
+const map = new Map() // stores unique value
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
 map.set('IN', "India")
 
-
-// console.log(map);
+console.log(map);
 
 for (const [key, value] of map) {
     // console.log(key, ':-', value);
